5b58bac7696c8717f19a87ef37c00e01bf4a7063
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-06-19 17:15:32 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3027,13 +3027,21 @@ class PlayState extends MusicBeatState
 			FlxTween.tween(scoreTxt, {color:0xFFFFFFFF}, 0.03);
 		}
 
-		if (healthBar.percent > 80)
-			iconP2.animation.curAnim.curFrame = 1;
-		else if (healthBar.percent < 20)
+		if (health > 2)
+			health = 2;
+		if (healthBar.percent < 20){
+			iconP1.animation.curAnim.curFrame = 1;
 			iconP2.animation.curAnim.curFrame = 2;
+			}
 		else
+		if (healthBar.percent > 80){
+			iconP2.animation.curAnim.curFrame = 1;
+			iconP1.animation.curAnim.curFrame = 2;
+		}
+		else{
 			iconP2.animation.curAnim.curFrame = 0;
-
+			iconP1.animation.curAnim.curFrame = 0;
+		}
 
 		if (FlxG.keys.anyJustPressed(debugKeysCharacter) && !endingSong && !inCutscene) {
 			persistentUpdate = false;
