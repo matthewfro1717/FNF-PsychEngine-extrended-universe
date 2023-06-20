e2959ccaa53c1b82fbee5f0d514ac0e9e05a38f4
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-06-20 07:07:41 -0400

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -34,7 +34,7 @@ class HealthIcon extends FlxSprite
 		else changeIcon('bf');
 	}
 
-	private var iconOffsets:Array<Float> = [0, 0];
+	private var iconOffsets:Array<Float> = [0, 0, 0];
 	public function changeIcon(char:String) {
 		if(this.char != char) {
 			var name:String = 'icons/' + char;
@@ -46,6 +46,7 @@ class HealthIcon extends FlxSprite
 			loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr
 			iconOffsets[0] = (width - 150) / 2;
 			iconOffsets[1] = (width - 150) / 2;
+			iconOffsets[2] = (width - 150) / 2;
 			updateHitbox();
 
 			animation.add(char, [0, 1, 2], 0, false, isPlayer);
