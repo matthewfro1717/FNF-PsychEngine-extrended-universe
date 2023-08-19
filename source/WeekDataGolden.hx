50b447be1fd0dd610ebf1f3d133627ee28b1eddb
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-08-19 17:59:41 -0400

    Update WeekDataGolden.hx

--- source/WeekDataGolden.hx
+++ source/WeekDataGolden.hx
@@ -14,18 +14,6 @@ using StringTools;
 typedef WeekFile =
 {
 	// JSON variables
-	var songs:Array<Dynamic>;
-	var weekCharacters:Array<String>;
-	var weekBackground:String;
-	var weekBefore:String;
-	var storyName:String;
-	var weekName:String;
-	var freeplayColor:Array<Int>;
-	var startUnlocked:Bool;
-	var hiddenUntilUnlocked:Bool;
-	var hideStoryMode:Bool;
-	var hideFreeplay:Bool;
-	var difficulties:String;
 }
 
 class WeekDataGolden {
@@ -46,6 +34,19 @@ class WeekDataGolden {
 	public var hideStoryMode:Bool;
 	public var hideFreeplay:Bool;
 	public var difficulties:String;
+
+	var songs:Array<Dynamic>;
+	var weekCharacters:Array<String>;
+	var weekBackground:String;
+	var weekBefore:String;
+	var storyName:String;
+	var weekName:String;
+	var freeplayColor:Array<Int>;
+	var startUnlocked:Bool;
+	var hiddenUntilUnlocked:Bool;
+	var hideStoryMode:Bool;
+	var hideFreeplay:Bool;
+	var difficulties:String;
 
 	public var fileName:String;
 
