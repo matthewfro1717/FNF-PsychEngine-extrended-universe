94bf9b945a4d81673020a42d8a43246690717ffb
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-08-19 18:07:38 -0400

    Update WeekDataSecret.hx

--- source/WeekDataSecret.hx
+++ source/WeekDataSecret.hx
@@ -29,20 +29,6 @@ class WeekDataSecret {
 	public var hideStoryMode:Bool;
 	public var hideFreeplay:Bool;
 	public var difficulties:String;
-
-	var songs:Array<Dynamic>;
-	var weekCharacters:Array<String>;
-	var weekBackground:String;
-	var weekBefore:String;
-	var storyName:String;
-	var weekName:String;
-	var freeplayColor:Array<Int>;
-	var startUnlocked:Bool;
-	var hiddenUntilUnlocked:Bool;
-	var hideStoryMode:Bool;
-	var hideFreeplay:Bool;
-	var difficulties:String;
-
 	public var fileName:String;
 
 	public static function createWeekFile():WeekFile {
